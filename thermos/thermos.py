8800932062188db418b8384350274df3c57fc721
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-04-19 17:35:05 +0200

    Added custom error pages 404 and 500

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 class User:
     def __init__(self, firstname, lastname):
         self.firstname = firstname
@@ -19,9 +20,20 @@ class User:
 def index():
     return render_template('index.html', user=User('John', 'Blum'))
 
+
 @app.route('/add')
 def add():
     return render_template('add.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
