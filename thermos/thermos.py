ff57a43db0378629b99de28b20a90d5a95ab9906
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-05-04 21:14:31 +0200

    Added forms to validate user input.

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,6 +1,50 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+from forms import BookmarkForm
 
 app = Flask(__name__)
+bookmarks = []
+app.config['SECRET_KEY'] = '9\x83\xc96NZ;\x88\xb9\xbc*\x83{\xc1\x8e7;\x16\x06p\xfc\x07\x9a\xc6'
+
+
+def store_bookmark(url, description):
+    bookmarks.append(dict(
+        url=url,
+        description =description,
+        user="reindert",
+        date=datetime.utcnow()
+    ))
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', new_bookmarks=new_bookmarks(5))
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored bookmark '{}'".format(description))
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 
 class User:
@@ -13,27 +57,6 @@ class User:
 
     def __str__(self):
         return "User: {} {}".format(self.firstname, self.lastname)
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', user=User('John', 'Blum'))
-
-
-@app.route('/add')
-def add():
-    return render_template('add.html')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def server_error(e):
-    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
