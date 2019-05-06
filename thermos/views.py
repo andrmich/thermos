41eecc0566ccac9d34711304e96fd5c122ee71e7
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-05-06 14:38:56 +0200

    Created sing up page, form, view

--- thermos/views.py
+++ thermos/views.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_required, login_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_required, login_user, logout_user, current_user
 
 from thermos import app, db, login_manager
-from thermos.forms import BookmarkForm, LoginForm
+from thermos.forms import BookmarkForm, LoginForm, SignupForm
 from thermos.models import User, Bookmark
 
 
@@ -24,10 +24,10 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        bm = Bookmark(user=logged_in_user(), url=url, description=description)
+        bm = Bookmark(user=current_user, url=url, description=description)
         db.session.add(bm)
         db.session.commit()
-        flash("Stored bookmark '{}'".format(description))
+        flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
 
@@ -42,13 +42,34 @@ def user(username):
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is not None:
+        user = User.get_by_username(form.username.data)
+        if user is not None and user.check_password(form.password.data):
             login_user(user, form.remember_me.data)
-            flash("Logged in successfully as {}".format(user.username))
-            return redirect(request.args.get('next') or url_for('index'))
-        flash("Incorrect username or password.")
+            flash("Logged in successfully as {}.".format(user.username))
+            return redirect(request.args.get('next') or url_for('user',
+                                                username=user.username))
+        flash('Incorrect username or password.')
     return render_template("login.html", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Welcome, {}! Please login.'.format(user.username))
+        return redirect(url_for('login'))
+    return render_template("signup.html", form=form)
 
 
 @app.errorhandler(404)
@@ -57,5 +78,5 @@ def page_not_found(e):
 
 
 @app.errorhandler(500)
-def server_error(e):
+def page_not_found(e):
     return render_template('500.html'), 500
