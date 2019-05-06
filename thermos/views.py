dfd9d585258dc6efe58ce2fc43bc5bced1ac28c0
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-05-06 13:00:05 +0200

    Added the login form, login template and view.

--- thermos/views.py
+++ thermos/views.py
@@ -1,14 +1,14 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_required
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, login_user
 
-from thermos import app, db
-from thermos.forms import BookmarkForm
+from thermos import app, db, login_manager
+from thermos.forms import BookmarkForm, LoginForm
 from thermos.models import User, Bookmark
 
 
-# Fake login
-def logged_in_user():
-    return User.query.filter_by(username="bella").first()
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(int(userid))
 
 
 @app.route('/')
@@ -31,10 +31,24 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
 
+
 @app.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            login_user(user, form.remember_me.data)
+            flash("Logged in successfully as {}".format(user.username))
+            return redirect(request.args.get('next') or url_for('index'))
+        flash("Incorrect username or password.")
+    return render_template("login.html", form=form)
 
 
 @app.errorhandler(404)
